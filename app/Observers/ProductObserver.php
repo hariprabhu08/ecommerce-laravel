fffb3367fca6228248294e9b5c749dd0ae6bff2d
yeha98555 <yeha98555@gmail.com>
2023-01-18 03:03:27 +0800

    add product observer to observe product become on sale, and notify favorite_users

---
+++ app/Observers/ProductObserver.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Product;
+use App\Notifications\ProductSale;
+
+class ProductObserver
+{
+    /**
+     * Handle the Product "created" event.
+     *
+     * @param  \App\Models\Product  $product
+     * @return void
+     */
+    public function created(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Handle the Product "updated" event.
+     *
+     * @param  \App\Models\Product  $product
+     * @return void
+     */
+    public function updated(Product $product)
+    {
+        $currents = $product->getAttributes();
+        $changes = $product->getChanges();
+        $original = $product->getOriginal();
+        // on sale product
+        if (isset($changes['price']) && ($changes['price'] - $original['price'] < 0) && $currents['quantity']) {
+            foreach ($product->favorite_users as $user) {
+                $user->notify(new ProductSale($product));
+            }
+        }
+    }
+
+    /**
+     * Handle the Product "deleted" event.
+     *
+     * @param  \App\Models\Product  $product
+     * @return void
+     */
+    public function deleted(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Handle the Product "restored" event.
+     *
+     * @param  \App\Models\Product  $product
+     * @return void
+     */
+    public function restored(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Handle the Product "force deleted" event.
+     *
+     * @param  \App\Models\Product  $product
+     * @return void
+     */
+    public function forceDeleted(Product $product)
+    {
+        //
+    }
+}
